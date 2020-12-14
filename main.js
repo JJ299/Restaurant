033bc29ea0ea0075dd97ee757052d6bbfddd7c93
JJ299 <62095762+JJ299@users.noreply.github.com>
2020-12-14 02:35:30 -0500

    Added scroll top/header CSS

--- main.js
+++ main.js
@@ -42,3 +42,10 @@ function scrollActive(){
 
 window.addEventListener('scroll', scrollActive)
 
+function scrollHeader () {
+    const nav = document.getElementById('header')
+
+    if (this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll', scrollHeader)
